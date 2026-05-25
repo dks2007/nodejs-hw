11b889418ad75e510fa41b0068f841923ace7254
Dima Karpinskyi <dimakjkarpinski@gmail.com>
2026-05-25 17:51:53 +0200

    node_homeWork

--- src/server.js
+++ src/server.js
@@ -1,23 +1,34 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import pinoHttp from 'pino-http';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+app.use(pinoHttp());
 app.use(cors());
+app.use(express.json());
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
 });
 
 app.get('/notes/noteId', (req, res) => {
-  const { id_param } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${id_param}` });
+  const { nodeId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${nodeId}` });
 });
 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${res.method}`);
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
   next();
 });
 
