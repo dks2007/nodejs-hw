d27c219479877c0ab0410eb1e0287b894b042be5
Dima Karpinskyi <dimakjkarpinski@gmail.com>
2026-05-31 13:25:36 +0200

    Fix

--- src/models/note.js
+++ src/models/note.js
@@ -3,7 +3,7 @@ import { Schema, model } from 'mongoose';
 const noteSchema = new Schema(
   {
     title: { type: String, required: true, trim: true },
-    constent: { type: String, default: '', trim: true },
+    content: { type: String, default: '', trim: true },
     tag: {
       type: String,
       enum: [
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -11,8 +11,8 @@ const router = Router();
 
 router.get('/notes', getAllNotes);
 router.get('/notes/:noteId', getNoteById);
-router.get('/notes', createNote);
-router.get('/notes/:nodeId', deleteNote);
-router.get('/notes/:noteId', updateNote);
+router.post('/notes', createNote);
+router.delete('/notes/:noteId', deleteNote);
+router.patch('/notes/:noteId', updateNote);
 
 export default router;
